39c2c25f0fc38ffc9ca72829e1606bb6b92e2e90
Rodrigo Sebastian Alarcon <powerjujuy1@gmail.com>
2023-01-07 09:42:31 -0300

    problemas con el componente

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import template from './components/card.html';
 window.onload = function() {
     console.log('Hello world!!!');
     console.info('Its my first webpack :)');
+    document.getElementById("app").innerHTML = template;
   };
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-mode: 'development',
-entry: './src/index.js',
+mode: "development",
+entry: "./src/index.js",
 output: {
-path: path.resolve(__dirname, 'dist'),
-filename: 'bundle.js'
+path: path.resolve(__dirname, "dist"),
+filename: "bundle.js"
 },
-devServer: {
-  static:{
-    directory: path.join(process.cwd(), 'dist')
+module: {
+  rules: [
+  {
+  test: /\.html$/i,
+  loader: "html-loader",
+  options: {
+  interpolate: true},
   },
+  ],
+  },
+devServer: {
+  
+    contentBase: path.join(process.cwd(), 'dist'),
   compress: true,
   port: 8050
   },
 plugins:[new HtmlWebpackPlugin({
-  template: path.resolve(process.cwd(),"src","index.html") //add new line
+  template: path.resolve(process.cwd(), "src", "index.html") 
   })],
   stats: {
     children: true,
