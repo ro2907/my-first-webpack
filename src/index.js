ea2b81d8eb125c2d1b8fe5f1cd0e169612342ea4
Rodrigo Sebastian Alarcon <powerjujuy1@gmail.com>
2023-01-06 21:34:37 -0300

    proyecto con error

---
+++ src/index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    console.log('Hello world!!!');
+    console.info('Its my first webpack :)');
+  };
+  
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+mode: 'development',
+entry: './src/index.js',
+output: {
+path: path.resolve(__dirname, 'dist'),
+filename: 'bundle.js'
+},
+devServer: {
+  static:{
+    directory: path.join(process.cwd(), 'dist')
+  },
+  compress: true,
+  port: 8050
+  },
+plugins:[new HtmlWebpackPlugin({
+  template: path.resolve(process.cwd(),"src","index.html") //add new line
+  })],
+  stats: {
+    children: true,
   }
 };
